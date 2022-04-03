564c44f8870b4d015d107f33b480e70e26a1b872
James Remus <James.Remus@veteransunited.com>
2022-04-03 16:06:23 -0500

    initial program to count emails for each day

--- counter.py
+++ counter.py
@@ -1,7 +1,24 @@
 
 def countDayOfTheWeek():
     # This first line is provided for you
-    file_name = input("Enter a file name: ")    # end assignment
+    countEmails = dict()
+    words = []
+    days = []
+    file_name = input("Enter a file name: ")  
+    try:
+        file_hand = open(file_name)
+    except:
+        print("Unable to open file:", file_name)
+    for line in file_hand:
+        days = []
+        if line.startswith("From "):
+            words = line.split()
+            days.append(words[2])
+            for day in days: 
+                countEmails[day] = countEmails.get(day, 0) + 1
+            print (countEmails)
+            continue
+# end assignment
 
 ## if you want to test locally before you try to sync
 ## uncomment calculateAbsolute() and run > python payCalculator.py
